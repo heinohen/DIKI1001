f98391847a282a12dfafc5ab882dac2420c29b92
Henrik Heinonen <hohein@utu.fi>
2024-03-19 20:53:19 +0200

    din

--- sentiment_analysis/sent.py
+++ sentiment_analysis/sent.py
@@ -9,7 +9,7 @@ import requests
 nlp = spacy.load("en_core_web_sm")
 nlp.add_pipe("spacytextblob")
 df = pd.read_csv("urls.csv")
-urls = df["Adress"].tolist()
+urls = df["Address"].tolist()
 
 # Get text from url's
 for count, x in enumerate(urls):
@@ -50,25 +50,40 @@ for count, x in enumerate(urls):
     url_sent_score.append(sentiment)
 
     # words 
-    pos_words = []
-    neg_words = []
+    pos_words = dict()
+    neg_words = dict()
 
     for x in doc._.blob.sentiment_assessments.assessments:
+        word = x[0][0]
         if x[1] > 0:
-            pos_words.append(x[0][0])
+            if word not in pos_words.keys():
+                pos_words[word] = 1
+            else:
+                pos_words[word] += 1
         elif x[1] < 0:
-            neg_words.append(x[0][0])
+            if word not in neg_words.keys():
+                neg_words[word] = 1
+            else:
+                neg_words[word] += 1
         else:
             pass
     
-    print(f'{url_sent_score} \t {url_sent_label} \t {pos_words} \t {neg_words}')
+    print(f'Most frequent positive words:')
+    for i in sorted(pos_words.keys(), key = lambda x:pos_words[x], reverse = True)[:5]:
+        print(i, pos_words[i])
+    print("\n")
+    print(f'Most frequent negative words:')
+    for i in sorted(neg_words.keys(), key = lambda x:neg_words[x], reverse = True)[:5]:
+        print(i, neg_words[i])
+
     for x in enumerate(url_sent_score):
         if x[1] > 0:
-            print("\U0001F600")
+            print(f'{url_sent_score, sent_label}\U0001F600')
         elif x[1] < 0:
-            print("\U0001F92E")
+            print(f'{url_sent_score, sent_label}\U0001F92E')
         else:
             pass
+    print("\n")
 
 
 
